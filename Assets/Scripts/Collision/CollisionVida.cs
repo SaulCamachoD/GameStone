d44451243cb8a3aa68c3d3679b3b0668cffa9705
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-06-14 02:30:15 -0500

    Prueba

--- Assets/Scripts/Collision/CollisionVida.cs
+++ Assets/Scripts/Collision/CollisionVida.cs
@@ -12,7 +12,7 @@ public class CollisionVida : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("armaImpacto"))
+        if (other.gameObject.CompareTag("armaImpactos"))
         {
             if (anim !=null)
             {
