ccaf3ddd4ed54834882426ff23ff3091c521250d
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-06-14 02:08:34 -0500

    Colliders y anim

---
+++ Assets/Scripts/Collision/CollisionVida.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionVida : MonoBehaviour
+{
+    public int hp;
+    public int dañoArma;
+    public int dañoPuño;
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("armaImpacto"))
+        {
+            if (anim !=null)
+            {
+                anim.Play("AnimacionDamage");
+            }
+            hp -= dañoArma;
+        }
+        if (hp < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
