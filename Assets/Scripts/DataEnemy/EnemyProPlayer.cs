fdb24fadfe5d63b96b9749939103f00564a6c98c
John Arango Buitrago <john.arango496@pascualbravo.edu.co>
2024-06-14 00:44:23 -0500

    Boss Controller

--- Assets/Scripts/DataEnemy/EnemyProPlayer.cs
+++ Assets/Scripts/DataEnemy/EnemyProPlayer.cs
@@ -11,30 +11,39 @@ public class EnemyProPlayer : EnemyCharacter
     {
         _agent = agent;
         _animator = animation;
+        if(_animator != null) _animator.SetBool("vivo" , true);
     }
     public override void EstadoIdle()
     {
         base.EstadoIdle();
+        if(_animator !=null)_animator.SetFloat("velocidad", 0);
+        if(_animator !=null)_animator.SetBool("atacando", false);
         _agent.SetDestination(_agent.transform.position);
+        
         UnityEngine.Debug.Log("Kieto");
     }
     public override void EstadoSeguir()
     {
         base.EstadoSeguir();
+        if(_animator !=null)_animator.SetFloat("velocidad",1);
+        if(_animator !=null)_animator.SetBool("atacando",false);
         _agent.SetDestination(target.transform.position);
         
         //UnityEngine.Debug.Log("siguiendo endo");
     }
     public override void EstadoAtacar()
     {
+        
         base.EstadoAtacar();
         _agent.SetDestination(_agent.transform.position);
         _agent.transform.LookAt(target,Vector3.up);
-      
+        if(_animator !=null)_animator.SetFloat("velcidad",0);
+        if(_animator !=null)_animator.SetBool("atacando",true);
     }
     public override void EstadoMuerto()
     {
         base.EstadoMuerto();
+        if(_animator !=null)_animator.SetBool("vivo",false);
         _agent.enabled = false;
         
         //UnityEngine.Debug.Log("muriendo endo");
